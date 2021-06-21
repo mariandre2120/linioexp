139ccb6f27b2b56283d731696f5a7dffb303ccf0
unknown <ma.martinezd@alum.up.edu.pe>
2021-06-20 22:56:12 -0500

    commit final

--- linioexp/urls.py
+++ linioexp/urls.py
@@ -1,6 +1,7 @@
 """linioexp URL Configuration
+
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,11 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('main.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('main.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('productos', views.ProductListView, name='product-list'),
+    path('productos', views.ProductListView.as_view(), name='product-list'),
     path('productos/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
     path('registro/', views.RegistrationView.as_view(), name='register'),
     path('add_to_cart/<int:product_pk>', views.AddToCartView.as_view(), name='add-to-cart'),
@@ -12,4 +15,4 @@ urlpatterns = [
     path('checkout/<int:pk>', views.PedidoUpdateView.as_view(), name='pedido-update'),
     path('payment/', views.PaymentView.as_view(), name='payment'),
     path('complete_payment/', views.CompletePaymentView.as_view(), name='complete-payment'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
